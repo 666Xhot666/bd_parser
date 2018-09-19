d7e98c0df1d8b1a90abad29f671ccd62d9951579
666Xhot666 <36788406+666Xhot666@users.noreply.github.com>
2018-09-19 18:33:30 +0300

    Update parser.js

--- parser.js
+++ parser.js
@@ -3,14 +3,8 @@ const Path = require('path')
 const sqlite = require('sqlite3').verbose();
 const FileName = '/data/athlete_events.csv'
 const FilePath = Path.join(__dirname, FileName);
-const Result = require('./result.js');
-const FileNameOut = './1.json'
 const rep = /("\s*)|(\s\([-A-z,]*\))/g
 const spl = /,(?!0|\s|\s-|500|980)/g
-
-// process.argv.forEach((val, index) => {
-//   console.log(`${index}: ${val}`);
-// });
 
 var File = Fs.readFileSync(FilePath, {
   encoding: 'utf-8'
@@ -75,26 +69,26 @@ const db = new sqlite.Database('./data/olympic_history.db', sqlite.OPEN_READWRIT
 
   console.log('Connected to the olympic_history database.');
 
-  // for (var row in SportJson) {
-  //   db.run(`INSERT or REPLACE INTO sports(name) VALUES("${SportJson[row]}")`)
-  // }
-  // for (var row in EventJson) {
-  //   db.run(`INSERT or REPLACE INTO events(name) VALUES("${EventJson[row]}")`)
-  // }
-  // for (var row in AthletesJson) {
-  //   let AthletesRow = AthletesJson[row];
-  //   db.run(`INSERT or REPLACE INTO athletes(full_name,sex,age,params,team_id) VALUES("${AthletesRow[0]}","${AthletesRow[1]}","${AthletesRow[2]}","${AthletesRow[3]}","${AthletesRow[4]}")`)
-  // }
-  // for (var row in TeamJson) {
-  //  let TeamJsonRow = TeamJson[row];
-  //   if (TeamJsonRow[0] != void 0) db.run(`INSERT or REPLACE INTO teams(noc_name,name) VALUES("${TeamJsonRow[0]}","${TeamJsonRow[1]}")`)
-  // }
-  // for (var col in GameJson) {
-  //   let GameJsonCol = GameJson[col];
-  //
-  //   if (GameJsonCol[0].length != 0) db.run(`INSERT or REPLACE INTO games(year,season,city) VALUES("${col}","0","${GameJsonCol[0]}")`)
-  //   if (GameJsonCol[1].length != 0) db.run(`INSERT or REPLACE INTO games(year,season,city) VALUES("${col}","1","${GameJsonCol[1]}")`)
-  // }
+   for (var row in SportJson) {
+     db.run(`INSERT or REPLACE INTO sports(name) VALUES("${SportJson[row]}")`)
+   }
+   for (var row in EventJson) {
+     db.run(`INSERT or REPLACE INTO events(name) VALUES("${EventJson[row]}")`)
+   }
+   for (var row in AthletesJson) {
+     let AthletesRow = AthletesJson[row];
+     db.run(`INSERT or REPLACE INTO athletes(full_name,sex,age,params,team_id) VALUES("${AthletesRow[0]}","${AthletesRow[1]}","${AthletesRow[2]}","${AthletesRow[3]}","${AthletesRow[4]}")`)
+   }
+   for (var row in TeamJson) {
+    let TeamJsonRow = TeamJson[row];
+     if (TeamJsonRow[0] != void 0) db.run(`INSERT or REPLACE INTO teams(noc_name,name) VALUES("${TeamJsonRow[0]}","${TeamJsonRow[1]}")`)
+   }
+   for (var col in GameJson) {
+     let GameJsonCol = GameJson[col];
+ 
+     if (GameJsonCol[0].length != 0) db.run(`INSERT or REPLACE INTO games(year,season,city) VALUES("${col}","0","${GameJsonCol[0]}")`)
+     if (GameJsonCol[1].length != 0) db.run(`INSERT or REPLACE INTO games(year,season,city) VALUES("${col}","1","${GameJsonCol[1]}")`)
+  }
 
   var JsonId = {
     a: [],
@@ -123,7 +117,7 @@ const db = new sqlite.Database('./data/olympic_history.db', sqlite.OPEN_READWRIT
             JsonId.d.push(event.id)
 
             console.log(JsonId);
-            //db.run(`INSERT or REPLACE INTO results(athlete_id, game_id, sport_id, event_id, medal) VALUES("${JsonId.a}","${JsonId.b}","${JsonId.c}","${JsonId.d}","${Medal}") `)
+            db.run(`INSERT or REPLACE INTO results(athlete_id, game_id, sport_id, event_id, medal) VALUES("${JsonId.a}","${JsonId.b}","${JsonId.c}","${JsonId.d}","${Medal}") `)
 
           })
         })
@@ -132,5 +126,4 @@ const db = new sqlite.Database('./data/olympic_history.db', sqlite.OPEN_READWRIT
 
   }
 
-  //db.close();
 });
