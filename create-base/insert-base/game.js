52878f5f7468af0d29cb4ab6fb7b55c774841c3b
666Xhot666 <36788406+666Xhot666@users.noreply.github.com>
2018-10-29 20:34:49 +0200

    Optimized

--- create-base/insert-base/game.js
+++ create-base/insert-base/game.js
@@ -1,18 +1,14 @@
 // game-table-insert
-const listId = {};
-
 exports.insertTable = (data, clear) => {
   Object.keys(data).forEach((row, id) => {
     clear.writeToBase({
       table: 'games',
       data: {
-        id: id + 1,
+        id: data[row].id,
         year: data[row].Year,
         season: data[row].Season,
         city: data[row].City,
       },
     });
-    listId[row] = id + 1;
   });
-  return listId;
 };
--- create-base/insert-base/teams.js
+++ create-base/insert-base/teams.js
@@ -1,17 +1,13 @@
 // team-table-insert
-const listId = {};
-
 exports.insertTable = (data, clear) => {
   Object.keys(data).forEach((row, id) => {
     clear.writeToBase({
       table: 'teams',
       data: {
-        id: id + 1,
+        id: data[row].id,
         noc_name: row,
-        name: data[row],
+        name: data[row].team,
       },
     });
-    listId[row] = id + 1;
   });
-  return listId;
 };
